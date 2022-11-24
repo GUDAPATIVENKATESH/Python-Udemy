fb8d7c996d3ecb474c76bc7d8968f9a255f08d29
Venkatesh <gudapativenkatesh4@gmail.com>
2022-11-24 20:51:46 +0530

    added RemaimgDays

---
+++ RemainingDays.py
@@ -0,0 +1,11 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+age_int = int(age)
+Remaining = (90 - age_int)
+Days = Remaining * 365
+Weeks = Remaining * 52
+Months = Remaining * 12
+print(f"You had {Days} Days, {Weeks} Weeks and {Months} Months in your Life If you Live till 90 Years")
