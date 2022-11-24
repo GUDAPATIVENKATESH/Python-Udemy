5cfe628b00f1c9a384dcfb0b04ee6113f3804a8e
Venkatesh <gudapativenkatesh4@gmail.com>
2022-11-24 08:50:32 +0530

    exercice

--- EXE.py
+++ EXE.py
@@ -1,1 +1,6 @@
-print('Day 1 - String Manipulation\nString Concatenation is done with the "+" sign.\ne.g. print("Hello " + "world")\nNew lines can be created with a backslash and n.')
+Number = input("Enter a Double Digit Number:")
+# print(type(Number))
+First = int(Number[0])
+Second = int(Number[1])
+Sum = First + Second
+print(f"The Sum is {Sum}")
